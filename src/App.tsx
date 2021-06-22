8f598c2d14ad33312dcfd80146f4116fbb2e5678
Eugen <krizhik@gmail.com>
2021-06-22 22:11:35 +0300

    10_lessons

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.css';
 import {Header} from "./components/Header/Header";
 import {Technologies} from "./components/Technologies/Technologies";
+import Footer from "./components/Footer/Footer";
+
+
+
 
 const App = () => {
     return (
         <div>
             <Header/>
             <Technologies/>
+            <Footer/>
         </div>
     );
 }
